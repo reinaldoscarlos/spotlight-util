53287e472453b65680c05f5106ed925ad6588d5e
reinaldoscarlos <reinaldoscarlos@gmail.com>
2013-05-03 02:59:21 -0300

    Correction of implementation errors:
    The code was changed to return the label of the object and not the entire URI.
    The code of the class StatementContextExtractor was changed to have subclasses according to the variable partToBeExtracted.
    A visitor was developed in class ObjectExtractor to eliminate the use of ifs.
    The name of the main class was changed to ContextExtractor and its comment.
    .gitignore and .gitattributes were added, copied from the project DBpedia spotlight.
    The extraction of the type of an object is unnecessary and was removed. It was developed to return if an object is a URI, a literal or anonymous.

--- src/main/scala/ContextExtractor.scala
+++ src/main/scala/ContextExtractor.scala
@@ -7,13 +7,13 @@ import org.apache.lucene.analysis.TokenStream
 import org.apache.lucene.util.Version
 
 /**
- * Created with IntelliJ IDEA.
- * User: reinaldo
- * Date: 26/04/13
- * Time: 09:57
- * To change this template use File | Settings | File Templates.
+ * Context Extraction from RDF
+ * Used to process the properties and labels and group them by the subject
+ * The output is formatted as TSV or JSON
+ *
+ * @author reinaldo
  */
-object TestingJenaScala extends App {
+object ContextExtractor extends App {
 
   /**
    * Choose the format of the output
@@ -25,22 +25,19 @@ object TestingJenaScala extends App {
   val format = "TSV"
   //val format = "JSON"
 
-  //object extraction
-  extraction("object", format, "files/inputs/3.8_sl_en_sl_labels_en.nt", "files/VALUE_OF_OBJECT_3.8_sl_en_sl_labels_en.tsv")
-  extraction("object", format, "files/inputs/3.8_sl_en_sl_mappingbased_properties_en.nt", "files/VALUE_OF_OBJECT_3.8_sl_en_sl_mappingbased_properties_en.tsv")
-  extraction("object", format, "files/inputs/3.8_sl_en_sl_instance_types_en.nt","files/VALUE_OF_OBJECT_3.8_sl_en_sl_instance_types_en.tsv")
+  //Extracting the label of the object
+  labelExtraction("object", format, "files/inputs/3.8_sl_en_sl_labels_en.nt", "files/VALUE_OF_OBJECT_3.8_sl_en_sl_labels_en.tsv")
+  labelExtraction("object", format, "files/inputs/3.8_sl_en_sl_mappingbased_properties_en.nt", "files/VALUE_OF_OBJECT_3.8_sl_en_sl_mappingbased_properties_en.tsv")
+  labelExtraction("object", format, "files/inputs/3.8_sl_en_sl_instance_types_en.nt","files/VALUE_OF_OBJECT_3.8_sl_en_sl_instance_types_en.tsv")
 
-  //property extraction
-  extraction("property", format, "files/inputs/3.8_sl_en_sl_mappingbased_properties_en.nt", "files/VALUE_OF_PROPERTY_3.8_sl_en_sl_mappingbased_properties_en.tsv")
-
-  //type extraction
-  extraction("type", format, "files/inputs/3.8_sl_en_sl_mappingbased_properties_en.nt", "files/VALUE_OF_TYPE_3.8_sl_en_sl_mappingbased_properties_en.tsv")
+  //Extracting the label of the property
+  labelExtraction("property", format, "files/inputs/3.8_sl_en_sl_mappingbased_properties_en.nt", "files/VALUE_OF_PROPERTY_3.8_sl_en_sl_mappingbased_properties_en.tsv")
 
   /*
   Reads in files into a Jena Model, performs context extraction, formatting and outputs context into a file.
   Context extraction can focus on property labels, object labels or object type labels.
   */
-  def extraction (partToBeExtracted: String, format: String, inputFile: String, outputFile: String) = {
+  def labelExtraction (partToBeExtracted: String, format: String, inputFile: String, outputFile: String) = {
     //Reading the input
     val source = new JenaStatementSource(inputFile)
     // creating the output
@@ -48,22 +45,17 @@ object TestingJenaScala extends App {
     // choosing output format
     val formatter = if (format.equals("TSV")) new TSVOutputFormatter else new PigOutputFormatter
     // choosing extraction strategy
-    val extractor = new StatementContextExtractor(partToBeExtracted)
+    val extractor = if (partToBeExtracted.equals("object")) new ObjectExtractor else new PropertyExtractor
     // applying over input
     source.groupBy( e => e.getSubject ).flatMap {
       case (subject,statements) => {
         val context = extractor.extract(statements).mkString(" ")
-        output.println(formatter.format(subject.getURI,context))
+        output.println(formatter.format(subject.getLocalName,context))
         Some((subject,context))
       }
       case _ => None
     }.toSeq
-     .sortBy( e => e._1.getURI )
-
+     .sortBy( e => e._1.getLocalName )
     output.close()
-
-
   }
-
-
 }
--- src/main/scala/StatementContextExtractor.scala
+++ src/main/scala/StatementContextExtractor.scala
@@ -1,30 +1,36 @@
-import com.hp.hpl.jena.rdf.model.Statement
+import com.hp.hpl.jena.rdf.model._
 
 /**
- * Temporary class that extracts different context definitions from a Jena Statement.
- * TODO refactor to have subclasses that behave according to "partToBeExtracted"
+ * Class that extracts different context definitions from a Jena Statement.
  */
-class StatementContextExtractor(partToBeExtracted: String) {
-  //TODO instead of a long "if", use subclasses with specific behaviors
-  def extract(property: Statement) : String = {
-    if (partToBeExtracted.equals("object"))
-      if (property.getObject.isLiteral)
-        property.getLiteral.getLexicalForm
-      else
-        property.getObject.toString //TODO get label
-    else if (partToBeExtracted.equals("property"))
-      property.getPredicate.getLocalName //TODO get label (label does not necessarily equal local name)
-    else { //TODO Pablo does not know what these below are for. Probable misinterpretation.
-      if(property.getObject.isLiteral)
-        "literal"
-      else if(property.getObject.isURIResource)
-        "named"
-      else
-        "anonynous"
-    }
-  }
-
+trait StatementContextExtractor {
+  def extract(property: Statement) : String
   def extract(statements: Traversable[Statement]) : Traversable[String] = {
     statements.map(s => extract(s))
+  }
+}
+
+class ObjectExtractor extends StatementContextExtractor {
+  def extract(property: Statement) : String = {
+    property.getObject.visitWith(new ObjectVisitor).toString
+  }
+
+  //Visitor used to get the label according to the object type
+  class ObjectVisitor extends RDFVisitor {
+    def visitBlank(node: Resource, arg1: AnonId): AnyRef = {
+      ""
+    }
+    def visitLiteral(literal: Literal): AnyRef = {
+      literal.getLexicalForm
+    }
+    def visitURI(resource: Resource, arg1: String): AnyRef = {
+      resource.getLocalName
+    }
+  }
+}
+
+class PropertyExtractor extends StatementContextExtractor {
+  def extract(property: Statement) : String = {
+    property.getPredicate.getLocalName  //TODO get label (label does not necessarily equal local name)
   }
 }
